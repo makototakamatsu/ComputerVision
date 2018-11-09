c6408869f1d5b138db9ce1096ca46bfa6579b684
高松　真 <sanfranc75@gmail.com>
2018-11-09 21:17:39 +0900

    Add files via upload

--- compare_rgb2depth/no1.m
+++ compare_rgb2depth/no1.m
@@ -2,11 +2,21 @@ clear all;
 %右側に写真が配置される(深度画像)
 file_name = uigetfile('*');
 IMG = imread(file_name);
+%バイラテラルフィルタ
+patch = imcrop(IMG,[50, 50, 50 50]);
+imshow(patch);
+patchVar = std2(patch)^2;
+DoS = 2*patchVar;
+IMG = imbilatfilt(IMG,DoS);
+imshow(IMG);
+IMG = imbilatfilt(IMG,DoS,2);
+imshow(IMG);
 IMG = imresize(IMG,[256 256]);
 IMG(:,:,1) = imadjust(IMG(:,:,1));
 IMG(:,:,2) = imadjust(IMG(:,:,1));
 IMG(:,:,3) = imadjust(IMG(:,:,1));
-IMG = histeq(IMG);
+%ヒストグラム
+%IMG = histeq(IMG);
 imagesc(IMG); colorbar; axis image;
 
 IMG0 = IMG;
